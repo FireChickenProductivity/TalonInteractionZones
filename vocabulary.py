eb67d2058d36ec3f3950b851800b87cd32fd2dea
Fire Chicken <firechickenproductivity@gmail.com>
2025-12-22 14:03:18 -0700

    Provide more vocabulary actions

--- vocabulary.py
+++ vocabulary.py
@@ -32,6 +32,7 @@ def load_word_count_trie(path: str):
 	return trie
 
 class Vocabulary:
+	"""Offers word completion options"""
 	__slots__ = ('big_vocabulary', 'personal_vocabulary', 'personal_vocabulary_path')
 
 	def __init__(self):
@@ -45,15 +46,18 @@ class Vocabulary:
 			self.personal_vocabulary = Trie("")
 
 	def get_completions_for(self, text: str, limit: int):
+		"""Offer at most limit word completion options for the text given"""
 		return self.big_vocabulary.get_possibilities(
 			text.lower(),
 			limit
 		)
 
 	def handle_word_used_by_user(self, word: str):
+		"""Update personal vocabulary in response to a word used by the user"""
 		self.personal_vocabulary.add_text(word.lower(), 1, add_numbers)
 
 	def save_personal_vocabulary(self):
+		"""Save the personal vocabulary. This may be an expensive operation, so do not run after each word is used"""
 		save_word_count_trie(self.personal_vocabulary_path, self.personal_vocabulary)
 
 vocabulary: Vocabulary
@@ -73,4 +77,11 @@ class Actions:
 			limit: the maximum number of words to return
 		"""
 		return vocabulary.get_completions_for(text, limit)
-		
+	
+	def interaction_zones_handle_word_used_by_user(word: str):
+		"""Update vocabulary data in response to the user typing a word"""
+		vocabulary.handle_word_used_by_user(word)
+
+	def interaction_zones_save_vocabulary():
+		"""Saves the vocabulary to disk. This may be an expensive operation."""
+		vocabulary.save_personal_vocabulary()
